3318c2b75ac2f3e29699360f93c5ffcbeee4c8e6
Josh Di Fabio <joshdifabio@gmail.com>
2017-11-27 10:07:32 +0000

    Modify MapElements to use FlatMapElements

--- src/MapElements.php
+++ src/MapElements.php
@@ -1,31 +1,21 @@
 <?php
 namespace Joshdifabio\Transform;
 
-final class MapElements implements Transform
+final class MapElements
 {
     use FluentTransformTrait;
 
     public static function via(callable $fn): Transform
     {
-        $transform = new self;
-        $transform->fn = $fn;
-        return $transform;
+        return FlatMapElements::via(function ($element) use ($fn) {
+            yield $fn($element);
+        });
     }
 
     public static function viaMethod(string $methodName): Transform
     {
-        return MapElements::via(function ($element) use ($methodName) {
-            return $element->$methodName();
+        return FlatMapElements::via(function ($element) use ($methodName) {
+            yield $element->$methodName();
         });
     }
-
-    public function applyTo($input): \Iterator
-    {
-        assertIterable($input);
-        foreach ($input as $inputElement) {
-            yield ($this->fn)($inputElement);
-        }
-    }
-
-    private $fn;
 }
